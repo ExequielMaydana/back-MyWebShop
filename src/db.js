73d20b74fd97a17efe6affe7643b458b883c3479
ExequielMaydana <exedevcoding22@gmail.com>
2023-11-12 17:52:03 -0300

    edit db url

--- src/db.js
+++ src/db.js
@@ -1,22 +1,22 @@
 const mongoose = require("mongoose");
 
-// mongoose
-//   .connect(`${process.env.BD_URL}`, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     serverSelectionTimeoutMS: 5000,
-//   })
-//   .then((db) => console.log("Db is connected"))
-//   .catch((error) => console.log(error));
-
 mongoose
-  .connect(
-    `mongodb+srv://closetwithoutgender:megustaelarte@closetwithoutgender.dcdm7bq.mongodb.net/`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000,
-    }
-  )
+  .connect(`${process.env.BD_URL}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+  })
   .then((db) => console.log("Db is connected"))
   .catch((error) => console.log(error));
+
+// mongoose
+//   .connect(
+//     `mongodb+srv://closetwithoutgender:megustaelarte@closetwithoutgender.dcdm7bq.mongodb.net/`,
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       serverSelectionTimeoutMS: 5000,
+//     }
+//   )
+//   .then((db) => console.log("Db is connected"))
+//   .catch((error) => console.log(error));
