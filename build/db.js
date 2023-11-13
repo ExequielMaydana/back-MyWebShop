48cae00497b680303332c558a18f5c794eaf8272
Maydana Hernan Exequiel <exedevcoding22@gmail.com>
2023-11-13 09:02:52 -0300

    F/todo (#4)
    * add controller search product
    
    * edit db url
    
    * delete db uri
    
    * build

--- build/db.js
+++ build/db.js
@@ -12,10 +12,13 @@ mongoose.connect("".concat(process.env.BD_URL), {
 });
 
 // mongoose
-//   .connect(`mongodb://localhost:27017/store_closetwithoutgender`, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     serverSelectionTimeoutMS: 5000,
-//   })
+//   .connect(
+//     ``,
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       serverSelectionTimeoutMS: 5000,
+//     }
+//   )
 //   .then((db) => console.log("Db is connected"))
 //   .catch((error) => console.log(error));
